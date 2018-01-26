a15c8afabeaba9510b8117066f0ce03c38af38c7
Alexander Momotiuk <alex.m@ualeaders.com>
2018-01-26 14:59:11 +0200

    refactoring added scrolling

--- AMGiphyPicker/Source/Model/AMGifPickerModel.swift
+++ AMGiphyPicker/Source/Model/AMGifPickerModel.swift
@@ -15,35 +15,102 @@ fileprivate struct Configuration {
 protocol AMGifPickerModelDelegate: class {
     
     func modelDidUpdatedData(_ model: AMGifPickerModel)
+    func model(_ model: AMGifPickerModel, didInsert indexPath: [IndexPath])
 }
 
 class AMGifPickerModel {
     
-    weak var dlelegate: AMGifPickerModelDelegate?
+    weak var delegate: AMGifPickerModelDelegate?
     
     private var configuration: AMGifPickerConfiguration
     
-    private var trendingGifs: Set<AMGifViewModel> = []
-    private var searchGifs: Set<AMGifVideModel> = []
+    private var trendingGifs: [AMGifViewModel] = []
+    private var searchGifs: [AMGifViewModel] = []
+    
     private var searchString: String? = nil
+    private var isLoading: Bool = false
     
     private var provider: AMGifDataProvider
     
     init(config: AMGifPickerConfiguration) {
         configuration = config
         provider = AMGifDataProvider(apiKey: config.apiKey)
+        
+        AMGifCacheManager.shared.cleanCache()
+        
         loadData()
     }
 }
 
-//MARK: - Private Methods
+//MARK: - Fetching Data Methods
 extension AMGifPickerModel {
     
+    //First Fetching
     private func loadData() {
+        isLoading = true
         provider.loadGiphy(nil, offset: 0, limit: Configuration.limit) {[weak self] (gifs) in
+            self?.isLoading = false
             guard let gifs = gifs, let strongSelf = self else { return }
-            strongSelf.trendingGifs = strongSelf.trendingGifs.union(gifs)
-            strongSelf.dlelegate?.modelDidUpdatedData(strongSelf)
+            strongSelf.appendTrending(gifs)
+            strongSelf.delegate?.modelDidUpdatedData(strongSelf)
+        }
+    }
+    
+    private func appendTrending(_ gifs: [AMGif]) {
+        let gifsViewModel = gifs.map { return AMGifViewModel.init($0) }
+        self.trendingGifs.append(contentsOf: gifsViewModel)
+        //self.delegate?.modelDidUpdatedData(self)
+    }
+    
+    func search(_ search: String?) {
+        if let newSearch = search {
+            if newSearch != searchString {
+                searchString = newSearch
+                provider.loadGiphy(newSearch, offset: 0, limit: Configuration.limit, completion: {[weak self] (newGifs) in
+                    guard let gifs = newGifs, let strongSelf = self else { return }
+                    let gifsViewModel = gifs.map { return AMGifViewModel.init($0) }
+                    strongSelf.searchGifs = gifsViewModel
+                    strongSelf.delegate?.modelDidUpdatedData(strongSelf)
+                })
+            }
+        } else {
+            //Clean search cache and cancel all loading operation
+            searchGifs.forEach { $0.cancelFetching() }
+            searchGifs.removeAll()
+            
+            //If trending gifs already exist
+            if trendingGifs.count >= Configuration.limit {
+                delegate?.modelDidUpdatedData(self)
+            } else {
+                provider.loadGiphy(limit: Configuration.limit, completion: {[weak self] (newGifs) in
+                    guard let gifs = newGifs, let strongSelf = self else { return }
+                    strongSelf.appendTrending(gifs)
+                })
+            }
+        }
+    }
+    
+    func loadNext() {
+        if let search = searchString, searchGifs.count < configuration.maxLoadCount {
+            let limit = configuration.maxLoadCount - searchGifs.count > Configuration.limit ? Configuration.limit : configuration.maxLoadCount - searchGifs.count
+            provider.loadGiphy(search, offset: searchGifs.count, limit: limit, completion: {[weak self] (newGifs) in
+                guard let gifs = newGifs, let strongSelf = self else { return }
+                let gifsViewModel = gifs.map { return AMGifViewModel.init($0) }
+                strongSelf.searchGifs.append(contentsOf: gifsViewModel)
+                strongSelf.delegate?.modelDidUpdatedData(strongSelf)
+            })
+        } else if trendingGifs.count < configuration.maxLoadCount {
+            let limit = configuration.maxLoadCount - trendingGifs.count > Configuration.limit ? Configuration.limit : configuration.maxLoadCount - trendingGifs.count
+            provider.loadGiphy(nil, offset: trendingGifs.count, limit: limit, completion: {[weak self] (newGifs) in
+                guard let gifs = newGifs, let strongSelf = self else { return }
+                let startIndex = strongSelf.trendingGifs.count
+                strongSelf.appendTrending(gifs)
+                var indexes: [IndexPath] = []
+                for (index, _) in gifs.enumerated() {
+                    indexes.append(IndexPath(row: startIndex + index, section: 0))
+                }
+                strongSelf.delegate?.model(strongSelf, didInsert: indexes)
+            })
         }
     }
 }
@@ -58,11 +125,10 @@ extension AMGifPickerModel {
         return trendingGifs.count
     }
     
-    func item(at index: Int) -> AMGifViewModel
+    func item(at index: Int) -> AMGifViewModel? {
+        if searchString != nil {
+            return searchGifs[index]
+        }
+        return trendingGifs[index]
+    }
 }
-
-
-
-
-
-
